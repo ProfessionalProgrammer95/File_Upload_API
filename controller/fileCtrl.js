ebbd48c42582de2612c241a42c6267ad3bad495f
ProfessionalProgrammer95 <rakshithav1358@gmail.com>
2024-03-05 11:38:04 +0530

    file_updated

--- controller/fileCtrl.js
+++ controller/fileCtrl.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require('http-status-codes')
 const File = require('../model/file')
+const fs = require('fs')
 
 //upload File - post
 const uploadFile =async (req,res) => {
@@ -54,6 +55,7 @@ const deleteFile =async (req,res) => {
         if(!extFile)
             return res.status(StatusCodes.NOT_FOUND).json({status: false, msg:'requested id not found'})
 
+            fs.unlinkSync(extFile.path) /* delete file from location*/
             await File.findByIdAndDelete(id)
 
         res.status(StatusCodes.ACCEPTED).json({ status:true, msg : ' File Deleted Successfully'})
